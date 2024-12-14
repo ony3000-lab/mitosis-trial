b6e0397553624a8fc092966395c6c547398e34fc
Hyeonjong <nianelo4@gmail.com>
2024-12-14 21:21:26 +0900

    Create new mitosis project

---
+++ test-apps/react/src/App.jsx
@@ -0,0 +1,13 @@
+import { AutoComplete, Todos } from '@mitosis-trial/library-react';
+
+function App() {
+  return (
+    <div>
+      <div>Welcome to React.</div>
+      <AutoComplete />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
